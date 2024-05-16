687f002409a68af73e87bdd1446d3d753cf01e98
galleonpt <jose15.luis00@gmail.com>
2024-05-17 00:04:06 +0100

    chore: enable documentation for all stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,5 +15,8 @@ const config: StorybookConfig = {
         options: {},
     },
     staticDirs: ["../public"],
+    docs: {
+        defaultName: "Documentation",
+    },
 };
 export default config;
