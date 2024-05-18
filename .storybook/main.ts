7d52911aa099486ad1dc582bcfddcf4c3f350081
galleonpt <jose15.luis00@gmail.com>
2024-05-18 01:17:51 +0100

    export button on index file

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -18,17 +18,5 @@ const config: StorybookConfig = {
     docs: {
         defaultName: "Documentation",
     },
-    webpackFinal: async (config) => {
-        config.module?.rules?.push({
-            test: /\.(ts|tsx)$/,
-            use: [
-                {
-                    loader: require.resolve("ts-loader"),
-                },
-            ],
-        });
-        config.resolve?.extensions?.push(".ts", ".tsx");
-        return config;
-    },
 };
 export default config;
--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-function App() {
-    return <h1>Run npm run storybook</h1>;
-}
-
-export default App;
